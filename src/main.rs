8a1ae1bd4509ac6f26e0bf8c7860667067c59dc6
Volá Povírk <volapovirk@rambler.ru>
2023-08-17 17:09:56 -0400

    Add a /health_check endpoint handler
    This is a basic health check exposed by our service
    
    Signed-off-by: Volá Povírk <volapovirk@rambler.ru>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use tide::Request;
+use tide::Response;
+use tide::StatusCode;
+
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    let mut app = tide::new();
+    app.at("/health_check").get(health_check);
+    app.listen("127.0.0.1:8080").await?;
+    Ok(())
+}
+
+async fn health_check(_req: Request<()>) -> tide::Result {
+    Ok(Response::new(StatusCode::Ok))
 }
