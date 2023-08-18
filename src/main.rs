cf52c292d642d26e346fc6d4edb429ad6d83fb89
Volá Povírk <volapovirk@rambler.ru>
2023-08-18 17:13:25 -0400

    Refactored into a binary and a library
    Signed-off-by: Volá Povírk <volapovirk@rambler.ru>

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,8 @@
-use tide::Request;
-use tide::Response;
-use tide::StatusCode;
+use z2pgh::run;
 
 #[async_std::main]
-async fn main() -> tide::Result<()> {
-    let mut app = tide::new();
-    app.at("/health_check").get(health_check);
-    app.listen("127.0.0.1:8080").await?;
-    Ok(())
-}
+async fn main() -> std::io::Result<()> {
+    run().await?;
 
-async fn health_check(_req: Request<()>) -> tide::Result {
-    Ok(Response::new(StatusCode::Ok))
+    Ok(())
 }
