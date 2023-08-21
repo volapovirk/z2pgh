2316f9d98100959f471a85750bbec073baef8fc7
Volá Povírk <volapovirk@rambler.ru>
2023-08-21 13:34:00 -0400

    Add integration tests to cover /subscriptions
    Signed-off-by: Volá Povírk <volapovirk@rambler.ru>

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,27 @@
 use std::net::TcpListener;
+use tide::convert::{Deserialize, Serialize};
 use tide::Request;
 use tide::Response;
 use tide::StatusCode;
 
+#[derive(Debug, Serialize, Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
 pub async fn run(listener: TcpListener) -> std::io::Result<()> {
     let mut app = tide::new();
     app.at("/health_check").get(health_check);
+    app.at("/subscriptions").post(subscribe);
     app.listen(listener).await
 }
 
 async fn health_check(_req: Request<()>) -> tide::Result {
+    Ok(Response::new(StatusCode::Ok))
+}
+
+async fn subscribe(mut req: Request<()>) -> tide::Result {
+    let _data: FormData = req.body_form().await?;
     Ok(Response::new(StatusCode::Ok))
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,15 @@
 use std::net::TcpListener;
 
 use async_std::task;
+use surf::StatusCode;
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let _ = task::spawn(z2pgh::run(listener));
+
+    format!("http://127.0.0.1:{port}")
+}
 
 #[async_std::test]
 async fn health_check_works() {
@@ -17,10 +26,47 @@ async fn health_check_works() {
     assert!(response.is_empty().is_none());
 }
 
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    let port = listener.local_addr().unwrap().port();
-    let _ = task::spawn(z2pgh::run(listener));
+#[async_std::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let address = spawn_app();
 
-    format!("http://127.0.0.1:{port}")
+    let client = surf::Client::new();
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let request = surf::post(format!("{address}/subscriptions"))
+        .body(body)
+        .content_type("application/x-www-form-urlencoded");
+    let response = client
+        .send(request)
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(StatusCode::Ok, response.status());
+}
+
+#[async_std::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let address = spawn_app();
+    let client = surf::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+    for (invalid_body, error_message) in test_cases {
+        let request = surf::post(format!("{address}/subscriptions"))
+            .body(invalid_body)
+            .content_type("application/x-www-form-urlencoded");
+
+        let response = client
+            .send(request)
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            StatusCode::UnprocessableEntity,
+            response.status(),
+            "The API did not fail with 422 Unprocessable Content when the payload was {}.",
+            error_message
+        );
+    }
 }
