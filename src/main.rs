7f027138148b91534f3e9798853f6dd8e3f935fd
Volá Povírk <volapovirk@rambler.ru>
2023-08-24 10:26:22 -0400

    Introduced configuration management
    Signed-off-by: Volá Povírk <volapovirk@rambler.ru>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::net::TcpListener;
+use z2pgh::configuration::get_configuration;
 use z2pgh::startup::run;
 
 #[async_std::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000")?;
-    run(listener).await?;
-
-    Ok(())
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener).await
 }
