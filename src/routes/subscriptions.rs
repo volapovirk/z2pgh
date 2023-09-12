f76ea5427ada1f07341b290de8f8ba74c8ab295b
Volá Povírk <volapovirk@rambler.ru>
2023-09-12 15:26:41 -0400

    Added observability to our web application
    Signed-off-by: Volá Povírk <volapovirk@rambler.ru>

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,6 +3,7 @@ use tide::convert::{Deserialize, Serialize};
 use tide::Request;
 use tide::Response;
 use tide::StatusCode;
+use tracing::Instrument;
 use uuid::Uuid;
 
 use crate::state::State;
@@ -14,8 +15,19 @@ struct FormData {
 }
 
 pub async fn subscribe(mut req: Request<State>) -> tide::Result {
+    let request_id = Uuid::new_v4();
     let data: FormData = req.body_form().await?;
-    sqlx::query!(
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %data.email,
+        subscriber_name= %data.name
+    );
+    let _request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
+
+    match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
@@ -26,6 +38,13 @@ pub async fn subscribe(mut req: Request<State>) -> tide::Result {
         Utc::now()
     )
     .execute(req.state().db_pool.as_ref())
-    .await?;
-    Ok(Response::new(StatusCode::Ok))
+    .instrument(query_span)
+    .await
+    {
+        Ok(_) => Ok(Response::new(StatusCode::Ok)),
+        Err(e) => {
+            tracing::error!("Failed to execute query: {}", e);
+            Ok(Response::new(StatusCode::InternalServerError))
+        }
+    }
 }
