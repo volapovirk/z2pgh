01e546922ed795ce271a5531281f992a24cabfb5
Volá Povírk <volapovirk@rambler.ru>
2023-09-21 11:40:11 -0400

    Fix application host
    Signed-off-by: Volá Povírk <volapovirk@rambler.ru>

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,10 @@ async fn main() -> std::io::Result<()> {
         .acquire_timeout(std::time::Duration::from_secs(2))
         .connect_lazy(configuration.database.connection_string().expose_secret())
         .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application.port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
     run(listener, db_pool).await
 }
